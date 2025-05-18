83afb123b8e9ced69d625ca8e5c29136a78c8435
Nathan Oliveira <nathanmello2@outlook.com>
2025-05-18 17:53:02 -0300

    refactor: Configurar aplicação da textura em ambas faces do card blister.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Scene } from './components/Scene'
 function App() {
   return (
     <div className="w-screen h-screen bg-red-800">
-      <div className='flex justify-center items-center bg-black'>
-        <h1 className='text-red'>Teste tailwind</h1>
+      <div className='flex h-24 justify-center items-center bg-black'>
+        <h1 className='text-white font-bold'>Teste tailwind</h1>
       </div>
       <Scene />
     </div>
